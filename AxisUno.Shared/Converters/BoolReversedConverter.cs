e9a37af3791c9844e4510d897547597601748f58
SergeyRoznyuk <SergeyRoznyuk@gmail.com>
2022-04-19 17:47:16 +0300

    Added StateTriggers to button "Lock"

---
+++ AxisUno.Shared/Converters/BoolReversedConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AxisUno.Converters
+{
+    public class BoolReversedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return !(bool)value;
+        }
+    }
+}
